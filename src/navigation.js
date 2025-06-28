d70ef3c7ab73c56b8a74e10ac743746eb05bf462
nikita-kotliar <knr200710@gmail.com>
2025-06-28 17:48:52 +0200

    css

--- src/navigation.js
+++ src/navigation.js
@@ -1,10 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuItems = document.querySelectorAll(".header__menu li");
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.getElementById('header')
+  const burger = document.getElementById('burger')
+  const navMenu = document.getElementById('navMenu')
+  const menuItems = document.querySelectorAll('.header__menu > li')
 
-    menuItems.forEach(item => {
-      item.addEventListener("click", () => {
-        menuItems.forEach(el => el.classList.remove("active")); 
-        item.classList.add("active"); 
-      });
-    });
-  });
+  menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      menuItems.forEach((el) => el.classList.remove('active'))
+      item.classList.add('active')
+    })
+  })
+
+  burger?.addEventListener('click', () => {
+    navMenu?.classList.toggle('active')
+    header?.classList.toggle('active')
+  })
+
+  document.querySelectorAll('.header__menu a').forEach((link) =>
+    link.addEventListener('click', () => {
+      navMenu?.classList.remove('active')
+      header?.classList.remove('active')
+    })
+  )
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      header?.classList.add('activeScroll')
+    } else {
+      header?.classList.remove('activeScroll')
+    }
+  })
+})
